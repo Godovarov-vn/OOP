35e108a6287443f2c06ec39bde23bfd82856e6f1
Godovarov-vn <66126031+Godovarov-vn@users.noreply.github.com>
2022-01-12 00:45:40 +0300

    Update main.cs

--- logger/main.cs
+++ logger/main.cs
@@ -1,3 +1,7 @@
+/* 
+ Логгеры делают логи. Лог – это стандартизированный методы вывода информации о текущем состоянии системы.
+ */
+
 using System;
 using LoggerNamespace;
 using FactoryNamespace;
@@ -6,11 +10,11 @@ class Program
 {
     static void Main()
     {
-        ILogger logger = LoggerFactory.Create(LoggerTypes.FILE);
+        ILogger logger = LoggerFactory.Create(LoggerTypes.CONSOLE);         //если написать LoggerTypes.CONSOLE , то выведет в консоль сообщения. .FILE для вывода в файл
 
-        for(int i = 0; i < 4; i++)
-        {
-            logger.Message("message #" + (i + 1).ToString(), (MessageTypes)i);
+        for (int i = 0; i < 3; i++)                                         //просто цикл для того, чтобы продемонстрировать все типы сообщений
+        {            
+            logger.Message("message #" + (i + 1).ToString(), (MessageTypes)i);              //применяю метод Message который прописан в интерфейсе логгера, Message(строка сообщения, тип сообщения (см. MessageTypes.cs)) 
         }
     }
 }
