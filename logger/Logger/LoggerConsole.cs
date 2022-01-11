cb8c118b1528c13fefed966a981ba3f3c541fcc9
Godovarov-vn <66126031+Godovarov-vn@users.noreply.github.com>
2022-01-12 00:49:25 +0300

    Update LoggerConsole.cs

--- logger/Logger/LoggerConsole.cs
+++ logger/Logger/LoggerConsole.cs
@@ -4,21 +4,14 @@ namespace LoggerNamespace
 {
     class LoggerConsole : ILogger
     {
-        private static readonly ConsoleColor[] MessageTypeToColor = new[]{
-            ConsoleColor.White,
-            ConsoleColor.Yellow,
-            ConsoleColor.Red,
-            ConsoleColor.Red
-        };
-
         public override void Message(string message, MessageTypes type)
         {
-            Console.ForegroundColor = MessageTypeToColor[(int)type];
-            Console.WriteLine(
+            Console.WriteLine
+                (
                 (DateTime.Now).ToString() +
                 $"\t[{type.ToString()}]\t" +
                 message
-            );
-        } 
+                );
+        }
     }
 }
