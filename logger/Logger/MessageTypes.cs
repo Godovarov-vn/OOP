59a8ffb1489dac09dda8e5b379421c129da7de91
Godovarov-vn <66126031+Godovarov-vn@users.noreply.github.com>
2022-01-12 00:52:32 +0300

    Update MessageTypes.cs

--- logger/Logger/MessageTypes.cs
+++ logger/Logger/MessageTypes.cs
@@ -4,9 +4,8 @@ namespace LoggerNamespace
 {
     enum MessageTypes
     {
-        INFO    =0,
-        WARNING   =1,
-        ERROR   =2,
-        FATAL    =3
+        INFO = 0,
+        WARNING = 1,
+        ERROR = 2,
     }
 }
