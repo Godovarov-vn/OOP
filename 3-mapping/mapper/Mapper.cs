4d59d25e10b5a492d964fd170bc34262ff16e3e2
Godovarov-vn <66126031+Godovarov-vn@users.noreply.github.com>
2022-01-12 01:41:23 +0300

    Update Mapper.cs

--- 3-mapping/mapper/Mapper.cs
+++ 3-mapping/mapper/Mapper.cs
@@ -1,12 +1,14 @@
+//Мапперы занимаются конвертированием объекта из одного типа в другой
+
 using System;
 using DTONamespace;
 using DAONamespace;
 
 namespace MapperNamespace
 {
-    class Mapper : IMapper<DatabaseObject, DTO>
+    class Mapper : IMapper<DatabaseObject, DTO> //и в данном случае я создаю маппер, который из наших датабейс обжектов создает ДТО
     {
-        public DTO Map(DatabaseObject input)
+        public DTO Map(DatabaseObject input) //реализую метод Мар
         {
             var dto = new DTO();
 
