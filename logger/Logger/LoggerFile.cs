eb75edf4e4acc3c00220ac4320599406e863c44f
Godovarov-vn <66126031+Godovarov-vn@users.noreply.github.com>
2022-01-12 00:51:25 +0300

    Update LoggerFile.cs

--- logger/Logger/LoggerFile.cs
+++ logger/Logger/LoggerFile.cs
@@ -7,14 +7,14 @@ namespace LoggerNamespace
     {
         public const string FILE_PATH = "log.txt";
 
-        private static StreamWriter sw;
+        private static StreamWriter sw; //StreamWriter - уже реализованный в интернете класс, позволяет записывать в файл
 
         public LoggerFile()
         {
-            sw = new StreamWriter(FILE_PATH, append: true);
+            sw = new StreamWriter(FILE_PATH, append: true);     //тут мы создаём объект
         }
 
-        ~LoggerFile()
+        ~LoggerFile()       //метод завершения(как деструктор)
         {
             sw.Close();
         }
@@ -22,8 +22,8 @@ namespace LoggerNamespace
         public override void Message(string message, MessageTypes type)
         {
             sw.WriteLine(
-                (DateTime.Now).ToString() +
-                $"\t[{type.ToString()}]\t" +
+                (DateTime.Now).ToString() +         //выводим дату, которая конвертируется в строку,
+                $"\t[{type.ToString()}]\t" +        //тип и само сообщение
                 message
             );
         }
